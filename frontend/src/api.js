8c39ca0f9ba1be45eed6dfe98450eb8b44c5ca4e
SAI SUMANTH KUMAR <saisumanthkumar95509@gmail.com>
2021-11-10 09:57:41 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const API = 'https://djangochat951.herokuapp.com/api/api/message/'
-export const users = 'https://djangochat951.herokuapp.com/api/users/'
+export const API = 'https://localhost:8000/api/api/message/'
+export const users = 'https://localhost:8000/api/users/'
